bb582441c245f0fbd3910203502f204e56d25352
Joshua Burger <joshberger6969@outlook.com>
2022-06-05 21:24:47 -0400

    Import Component from react, Import connect from redux, Import getProducts from actions. Change function App to a class component. Connect component to redux global store and map state to props.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+
+import { Component } from 'react';
+import {connect} from 'react-redux'
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { getProducts } from './actions/Products'
 
-function App() {
-  return (
+class App extends Component {
+
+  componentDidMount(){
+    this.props.getProducts()
+  }
+
+
+  render (){
+    return (
     <div className="App">
       <Header />
       <Home />
     </div>
-  );
+    )
+  };
+
+
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    products: state.products,
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps, { getProducts })(App);
