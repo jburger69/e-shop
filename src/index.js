0e569f6e2d41bffbbd7e2dfdd739acd163161134
Joshua Burger <joshberger6969@outlook.com>
2022-06-05 21:06:10 -0400

    Import redux, Provider from react-redux, Products reducer, Thunk. Create global store and wrap around top level app.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import ProductsReducer from './reducers/ProductsReducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(ProductsReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store} >
     <App />
+    </Provider>
   </React.StrictMode>
 )
