80940b09fdff695f27b538c34a6ff24257a2e835
Joshua Burger <joshberger6969@outlook.com>
2022-06-06 23:20:52 -0400

    Change from functional component to class component, connect to redux store and map state to props, Create variable products to map over each product and display a productCard component with the deatils on a product.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import "../components/Home.css"
 import logo from "../images/pexels-karolina-grabowska-5632397.jpg";
+import ProductCard from '../components/ProductCard';
 
-function Home() {
-  return (
-    <div>
-        <div className="home">
-            <div className="home__container">
-                <img src={logo} alt="#" className='home__image'>
+class Home extends React.Component {
 
-                </img>
-            </div>
-        </div>
-    </div>
-  )
+
+  state={
+    products: []
+  }
+
+
+  render() {
+    const products = this.state.products.length === 0 ? this.props.products.map(p => <ProductCard post={p} />) 
+    : this.state.products.map(p => <ProductCard post={p} />)
+    console.log(this.props.products)
+    return (
+      <div>
+          <div className="home">
+              <div className="home__container">
+                  {/* <img src={logo} alt="#" className='home__image'> */}
+                  {products}
+              </div>
+          </div>
+      </div>
+    )
+  }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return{
+    products: state.products,
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps)(Home)
