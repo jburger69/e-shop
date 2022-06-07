4cf3823143d4bcdf4f0cc8cdd707d5adf019237a
Joshua Burger <joshberger6969@outlook.com>
2022-06-06 23:02:06 -0400

    Return dispatch with action and payload

--- src/actions/Products.js
+++ src/actions/Products.js
@@ -1,6 +1,15 @@
 
 
 export const getProducts = () => {
-    console.log("c")
     //Fetch products from fake API.
+    return (dispatch) => {
+        fetch("https://fakestoreapi.com/products")
+            .then(resp => resp.json())
+            .then(products => {
+                dispatch({
+                    type: "GET_PRODUCTS",
+                    payload: products
+                })
+            })
+        }
 }
