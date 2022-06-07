503ea254976d0ecfd062bd1a3f1b61a94a55f8d1
Joshua Burger <joshberger6969@outlook.com>
2022-06-06 23:32:01 -0400

    Change from function to class component, Deconstruct props to display product info

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ProductCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+class ProductCard extends Component {
+    render() {
+        const { image, id, title, category, description, price, rating} = this.props.product
+        return (
+            <div>
+                {title}<br></br>
+                {description}<br></br>
+                {category}<br></br>
+                {price}<br></br>
+                {rating.rate}<br></br>
+                <img src={image} alt="#"></img><br></br>
+            </div>
+        );
+    }
 }
 
 export default ProductCard;
+
