87ae6a6c91ab9adec11b1cb9d0a2d5060e3017ae
Joshua Burger <joshberger6969@outlook.com>
2022-06-07 17:27:58 -0400

    Remove old code and import card from material ui to display each product card.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import '../components/ProductCard.css'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 class ProductCard extends Component {
     render() {
         const { image, id, title, category, description, price, rating} = this.props.product
         return (
-            <div className='product__item'>
-                <h4>{title}</h4>
-                <h4>{description}</h4>
-                <h4>{category}</h4>
-                <h4>{price}</h4>
-                <h4>{rating.rate}</h4>
-                <img  className="product__item__img" src={image} alt="#"></img><br></br>
-            </div>
+            <Card sx={{ maxWidth: 450 }}>
+                <CardMedia
+                    component="img"
+                    height="300"
+                    image={image}
+                    alt="#"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {category}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        ${price}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {rating.rate}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Add to Cart</Button>
+                    <Button size="small">View Product</Button>
+                </CardActions>
+            </Card>
         );
     }
 }
