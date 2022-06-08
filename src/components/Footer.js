c56bd22d25466db32c0bb7b6a3671d21d127d54a
Joshua Burger <joshberger6969@outlook.com>
2022-06-08 13:41:54 -0400

    Create new Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../components/Footer.css"
+
+export default function Footer() {
+  return (
+    <div className='footer__bar'>
+        <span className='footer__text'>&copy; 2022 eShop</span><br></br>
+        <span className='footer__text'>Created by: Joshua Burger</span>
+    </div>
+  )
+}
