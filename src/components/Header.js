90ceee33381f85788f71cd76a8a34497e300c967
Joshua Burger <joshberger6969@outlook.com>
2022-08-17 12:53:23 -0400

    Change style decoration of header link to none, Create and wrap new checkout link around the cart so user can click to bring them to see their cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
     <div className='header'>
         <div className="header__logo">
         <StorefrontIcon className='header__logoImage' fontSize="large"/>
-        <Link to='/'>
+        <Link to='/' style={{ textDecoration: 'none' }}>
           <h2 className='header__logoTitle'>eShop</h2>
         </Link>
       </div>
@@ -29,10 +29,12 @@ function Header() {
             <span className="nav__itemLineOne">Your</span>
             <span className="nav__itemLineTwo">Shop</span>
           </div>
-          <div className="nav__itemBasket">
-            <ShoppingBasketIcon />
-            <span className="nav__itemLineTwo nav__basketCount">0</span>
-          </div>
+          <Link to='/checkout' style={{ textDecoration: 'none' }}>
+            <div className="nav__itemBasket">
+              <ShoppingBasketIcon />
+              <span className="nav__itemLineTwo nav__basketCount">0</span>
+            </div>
+          </Link>
       </div>
     </div>
     </>
